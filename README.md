8054f1f9655a6e34db94dc4adb3d35b384f41d52
cynosures <cynosurech@gmail.com>
2017-07-20 00:24:47 +0800

    update readme

--- README.md
+++ README.md
@@ -1,5 +1,54 @@
 # alipay-sdk-node
 
-## install
+[![JavaScript Style Guide](https://img.shields.io/badge/code_style-standard-brightgreen.svg)](https://standardjs.com)
 
-`npm install alipay-sdk-node --save`
+
+---
+## Install
+
+```bash
+ $ npm install alipay-sdk-node --save
+```
+---
+
+## Usage
+
+### Create Alipay Instance
+
+```js
+  const Alipay = require('alipay-sdk-node')
+  const alipay = new Aliapy({
+    app_id,
+    app_public_key,
+    app_private_key,
+    alipay_public_key,
+    sign_type = 'RSA2',
+    notify_url
+  })
+```
+
+### APP Pay
+* get the string of order info
+* params: biz_content [click](https://docs.open.alipay.com/204/105465/)
+```js
+  alipay.getOrderInfoStr({
+    subject: 'xxx',
+    out_trade_no: 'xxx',
+    total_amount: 'xxx',
+    product_code: 'xxx'
+  })
+```
+
+### Refund
+* params: biz_content [click](https://docs.open.alipay.com/api_1/alipay.trade.refund)
+* return: Promise Or Callback
+```js
+  alipay.refund({
+    out_trade_no: 'xxx',
+    trade_no: 'xxx',
+    refund_amount: 'xxx',
+    out_request_no: 'xxx',
+    refund_reason: 'xxx'
+  })
+```
+---
--- test/alipay.js
+++ test/alipay.js
@@ -1,14 +1,9 @@
+/* eslint-env mocha */
+
 'use strict'
 
+const assert = require('assert')
 const Alipay = require('../index')
-const alipay = new Alipay({
-  app_id: '2016080700188285',
-  app_public_key: 'appPublicKey',
-  app_private_key: `-----BEGIN RSA PRIVATE KEY-----\n${Buffer.from('appPrivateKey', 'ascii').toString('base64')}\n-----END RSA PRIVATE KEY-----`,
-  alipay_public_key: 'alipayPublicKey'
-})
-
-console.log(alipay)
 
 const params = {
   app_id: '2016080700188285',
@@ -25,26 +20,22 @@ const params = {
   version: '1.0',
   sign: 'e9zEAe4TTQ4LPLQvETPoLGXTiURcxiAKfMVQ6Hrrsx2hmyIEGvSfAQzbLxHrhyZ48wOJXTsD4FPnt+YGdK57+fP1BCbf9rIVycfjhYCqlFhbTu9pFnZgT55W+xbAFb9y7vL0MyAxwXUXvZtQVqEwW7pURtKilbcBTEW7TAxzgro='
 }
+const formattedParams = 'app_id=2016080700188285&biz_content={"subject":"goods","out_trade_no":"xxx","total_amount":"1.00","product_code":"QUICK_MSECURITY_PAY"}&charset=utf-8&method=alipay.trade.app.pay&sign=e9zEAe4TTQ4LPLQvETPoLGXTiURcxiAKfMVQ6Hrrsx2hmyIEGvSfAQzbLxHrhyZ48wOJXTsD4FPnt+YGdK57+fP1BCbf9rIVycfjhYCqlFhbTu9pFnZgT55W+xbAFb9y7vL0MyAxwXUXvZtQVqEwW7pURtKilbcBTEW7TAxzgro=&sign_type=RSA2&timestamp=2014-07-24 03: 07: 50&version=1.0'
 
 describe('Alipay', function () {
-  describe('#formatter', function () {
-    it('should get the exact sign string', function (done) {
-      const signStr = alipay.formatter(params)
-      console.log(signStr)
-      done()
+  describe('#constructor', function () {
+    it('appId is required', function () {
+      try {
+        new Alipay({})
+      } catch (err) {
+        assert.equal(err.code, 'ERR_ASSERTION')
+      }
     })
   })
 
-  describe('#getOrderInfoStr', function () {
-    it('should get the exact order info for app pay', function (done) {
-      const orderStr = alipay.getOrderInfoStr({
-        subject: 'goods',
-        out_trade_no: 'xxx',
-        total_amount: '1.00',
-        product_code: 'QUICK_MSECURITY_PAY'
-      })
-      console.log(orderStr)
-      done()
+  describe('#formatter', function () {
+    it('should get the exact sign string', function () {
+       assert.equal(Alipay.formatter(params), formattedParams)
     })
   })
 })
